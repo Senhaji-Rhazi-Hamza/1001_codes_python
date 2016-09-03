61c99d14c9d43d42fa4356fb82f27f47fb9e7fe4
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2016-09-03 02:26:50 +0200

    jacobi method implemented and tested

--- ALGEBRA/CH2_EX2/jacobi.py
+++ ALGEBRA/CH2_EX2/jacobi.py
@@ -1,11 +1,25 @@
+import math
 #################################################################################
-def mat_mult(A,B): #defining the multiply matrix
-  n = len(A)  
-  m = len(B[0])
+def mat_add(A,B): #defining addition matrix
+  n = len(A[0])
+  m = len(A)
   C = [[0 for i in range(n)] for j in range(m)]
   for i in range(n):
     for j in range(m):
-      for k in range(m):
+      C[j][i] = A[j][i] + B[j][i]
+  return C
+##################################################################################
+#################################################################################
+def mat_mult(A,B): #defining the multiply matrix
+  m = len(A)  
+  n = len(A[0])
+  n2 = len(B)
+  assert(n==n2),"impossible to multiply revise number of lines and coloumns" 
+  p = len(B[0])
+  C = [[0 for col in range(p)] for row in range(m)]
+  for i in range(m):
+    for j in range(p):
+      for k in range(n):
         C[i][j]+= A[i][k] * B[k][j]
   return C
 ##################################################################################
@@ -15,10 +29,16 @@ def jacobi(A,B): # Matrix A and vector B of Ax = B
   D = [ [A[i][j] if(i==j) else 0  for i in range(n) ] for j in range(n)]
   E = [ [-A[i][j] if(i < j) else 0  for i in range(n) ] for j in range(n)]
   F = [ [-A[i][j] if(i > j) else 0  for i in range(n) ] for j in range(n)]
+  tol = 10 ** -5
+  err = 10
+  it = 0
+  max_iter = 10000
   Un0 = [1 for i in range(n)]
   Un1 = [1 for i in range(n)]
-#  print(D)
-#  print(E)
-#  print(F)
-  return 9
+  while ((err > tol) & (it < max_iter)):
+    it = it + 1
+    Un1 = [ (1/A[i][i]) * (B[i] - sum(A[i][j] if(i!=j) else 0 for j in range(n) )) for i in range(n) ]
+    err = math.sqrt(sum([(Un0[i] - Un1[i])**2 for i in range(n)]))
+    Un0 = Un1
+  return Un1
 ##################################################################################
--- ALGEBRA/CH2_EX2/test.py
+++ ALGEBRA/CH2_EX2/test.py
@@ -1,6 +1,10 @@
 import jacobi
 #jacobi test
 ####################################################################
-A = [ [(i + j) for i in range(5) ] for j in range(5)]
-B = [1 for i in range(5) ]
-jacobi.jacobi(A,B)
+n = 5
+A = [ [1 if (i!= j) else n+1 for i in range(n) ] for j in range(n)]
+B = [1 for i in range(n) ]
+#A = [[3,-1,-1],[-1,3,1],[2,1,4]]
+#B = [1,3,7]
+#xjacobi expected = [1,1,1]
+print(jacobi.jacobi(A,B))
