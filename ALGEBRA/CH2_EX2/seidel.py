6be44a5fa184876ff4749c29b3813544b36bc55a
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2016-09-09 18:01:46 +0200

    method seidel implemented and tested

--- ALGEBRA/CH2_EX2/seidel.py
+++ ALGEBRA/CH2_EX2/seidel.py
@@ -24,11 +24,10 @@ def mat_mult(A,B): #defining the multiply matrix
   return C
 ##################################################################################
 #################################################################################
-def jacobi(A,B): # Matrix A and vector B of Ax = B
+def seidel(A,B): # Matrix A and vector B of Ax = B
   n = len(A)
-  D = [ [A[i][j] if(i==j) else 0  for i in range(n) ] for j in range(n)]
-  E = [ [-A[i][j] if(i < j) else 0  for i in range(n) ] for j in range(n)]
-  F = [ [-A[i][j] if(i > j) else 0  for i in range(n) ] for j in range(n)]
+  #P = [ [A[i][j] if(i <=j) else 0  for i in range(n) ] for j in range(n)]
+  #F = [ [-A[i][j] if(i > j) else 0  for i in range(n) ] for j in range(n)]
   tol = 10 ** -5
   err = 10
   it = 0
@@ -37,8 +36,9 @@ def jacobi(A,B): # Matrix A and vector B of Ax = B
   Un1 = [1 for i in range(n)]
   while ((err > tol) & (it < max_iter)):
     it = it + 1
+    Un1 =[(1/A[i][i]) * (B[i] - sum(A[i][j] * Un0[j] if i > j  else (A[i][j] * Un1[j] if i < j  else 0 ) for j in range(n) )) for i in range(n)]
+    #Un1 = [ (1/A[i][i]) * (B[i] - sum(A[i][j] *Un0[j] if(i!=j) else 0 for j in range(n) )) for i in range(n) ]
     #Un1 = [ (1/A[i][i]) * (B[i] - sum(A[i][j] if(i!=j) else 0 for j in range(n) )) for i in range(n) ]
-    Un1 = [ (1/A[i][i]) * (B[i] - sum(A[i][j] *Un0[j] if(i!=j) else 0 for j in range(n) )) for i in range(n) ]
     err = math.sqrt(sum([(Un0[i] - Un1[i])**2 for i in range(n)]))
     Un0 = Un1
   return Un1
--- ALGEBRA/CH2_EX2/test.py
+++ ALGEBRA/CH2_EX2/test.py
@@ -1,10 +1,14 @@
 import jacobi
+import seidel
 #jacobi test
 ####################################################################
 n = 5
 A = [ [1 if (i!= j) else n+1 for i in range(n) ] for j in range(n)]
 B = [1 for i in range(n) ]
-#A = [[3,-1,-1],[-1,3,1],[2,1,4]]
-#B = [1,3,7]
+A = [[3,-1,-1],[-1,3,1],[2,1,4]]
+B = [1,3,7]
 #xjacobi expected = [1,1,1]
-print(jacobi.jacobi(A,B))
+print("jacobi result ",jacobi.jacobi(A,B),"\n")
+A = [[3,-1,-1],[-1,3,1],[2,1,4]]
+B = [1,3,7]
+print("seidel result ",seidel.seidel(A,B),"\n")
