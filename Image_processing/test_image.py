c461bc217b2b9bc0a4acc3cc588b37212976e341
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2017-05-12 16:26:38 +0200

    dogs difference implemented, and youn display by show_dog(i, j)

--- Image_processing/test_image.py
+++ Image_processing/test_image.py
@@ -5,7 +5,9 @@ import image as im
 
 img = cv2.imread("ressources/cat.jpg",1)
 name = "ressources/cat.jpg"
+height, width = img.shape[:2]
+size = min(height, width)
+img = cv2.resize(img,(int(size / 2), int( size / 2)), interpolation = cv2.INTER_CUBIC)
 
-iobj = im.Image(name)
-iobj.show(3)
-
+imo = im.Image(name)
+imo.show_dog(2,2)
