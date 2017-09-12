d6fff822770cdd86aaa1bebfb86b71614f01daf7
Senhaji-Rhazi-Hamza <hamza.senhaji-rhazi@epita.fr>
2017-09-12 23:34:27 +0200

    change structure tree

--- ALGEBRA/CH2_EX1/test_cholesky.py
+++
@@ -1,4 +0,0 @@
-import cholesky
-A = [[1,1,1,1], [1,5,5,5],[1,5,14,14], [1,5,14,15]]
-
-print(cholesky.cholesky_get_l(A))
--- ALGEBRA/CH2_EX2/test_grad.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-def conjugate_gradient2(A,B):
-  n = len(A)
-  Un1 = [0 for i in range(n)] #initialize start vec
-  r = (B - np.dot(A, Un1))
-  X = r + Un1
-  Y = np.dot(X, -1)
-  return Y
